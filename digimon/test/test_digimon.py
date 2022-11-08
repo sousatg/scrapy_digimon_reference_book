ef6339d88493d6e58b68499e80a956b2aabfeb78
Gil Sousa <tiagosousafl@gmail.com>
2022-11-08 22:08:23 +0100

    Update test cases

--- digimon/test/test_digimon.py
+++ digimon/test/test_digimon.py
@@ -9,13 +9,25 @@ class DigimonSpiderTest(unittest.TestCase):
 
     def _test_item_results(self, results, expected_length=0):
         result = next(results)
+        self.assertIsNotNone(result['name'])
+        self.assertIsNotNone(result['url'])
         self.assertEqual(len(result), expected_length)
 
     def test_parse_digimon(self):
         results = self.spider.parse_digimon_page(fake_response_from_file('digimon/Beelzemon.html', 'https://digimon.fandom.com/wiki/Beelzemon'))
-        self._test_item_results(results, 20)
+        self._test_item_results(results, 18)
 
     def test_parse_page_with_image_in_name(self):
         results = self.spider.parse_digimon_page(fake_response_from_file('digimon/Dodomon.html', 'https://digimon.fandom.com/wiki/Dodomon'))
         self._test_item_results(results, 9)
-    
+
+    def test_parse(self):
+        url = 'https://digimon.fandom.com/wiki/Digimon_Reference_Book'
+        file_path = 'digimon/Digimon_Reference_Book.html'
+        results = list(self.spider.parse(fake_response_from_file(file_path, url)))
+
+        self.assertEqual(len(results), 1490)
+
+        for el in results:
+            self.assertIsNotNone(el.url)
+            
