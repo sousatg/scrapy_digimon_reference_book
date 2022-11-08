a7be11bf5ef9b8bd94fba65242358bdc023f979d
Gil Sousa <tiagosousafl@gmail.com>
2022-11-08 20:33:31 +0100

    Create custom processors for DigimonItemLoader

--- digimon/spiders/digimon_spider.py
+++ digimon/spiders/digimon_spider.py
@@ -1,9 +1,9 @@
 import scrapy
 import re
+import html2text
 from scrapy.loader import ItemLoader
 from digimon.items import DigimonItem
-from scrapy.loader.processors import TakeFirst
-
+from scrapy.loader.processors import Identity, TakeFirst, MapCompose
 
 XPATH_NAME = '//aside/h2/span[1]/text() | //aside/h2/text()[1]'
 XPATH_ORIGINAL_NAME = '//aside/h2//text()'
@@ -34,10 +34,50 @@ XPATH_GENDER = '//div[h3/text() = "Gender:"]/div'
 XPATH_ALIASES = '//div[h3/text() = "Aliases:"]/div'
 
 
+h = html2text.HTML2Text()
+h.ignore_links = True
+h.ignore_emphasis = True
+
+
+def replace_special_characters(text):
+    text = re.sub(r'##', '', text)
+    text = re.sub('\\\\', '', text)
+    text = re.sub('• ', '', text)
+
+    return text
+
+
+
+def split_strings(text):
+    lines = text.split('\n')
+
+    return lines
+
+def remove_reference_link_from_strings(text):
+    text = re.sub(r'\[\d*\]', '', text)
+
+    return text
+
+def strip_string(text):
+    return text.strip()
+
+def filter_empty_strings(text):
+    return None if len(text) == 0 else text
 
 class DigimonItemLoader(ItemLoader):
+    default_input_processor = MapCompose(
+        h.handle, replace_special_characters, remove_reference_link_from_strings, split_strings, strip_string, filter_empty_strings)
+
+    name_in = Identity()
     name_out = TakeFirst()
+
+    original_name_in = Identity()
+    original_name_out = Identity()
+
+    image_in = Identity()
     image_out = TakeFirst()
+
+    url_in = Identity()
     url_out = TakeFirst()
 
 
